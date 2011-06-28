630971023e74704d3e338ddcb0b4e3c7968382ac
Charles <charles@pidoco.com>
2011-06-28 18:53:14 +0200

    add offer into js

---
+++ js/hci.offers.js
@@ -0,0 +1,51 @@
+hci.offers = [
+  {
+    title : 'BMW sehr schnell',
+    description : 'Faehrt gut und schnell',
+    price : 16000,
+    place: 'Berlin - Wedding',
+    distance: 1
+  },
+  {
+    title : 'Billige Renault',
+    description : 'Vier raeder, ein Motor, kein Sitze',
+    price : 1500,
+    place: 'Erfurt',
+    distance: 500
+  },
+  {
+    title : 'Bequeme Audi',
+    description : 'Everyone dreams of an Audi.',
+    price : 17000,
+    place: 'Berlin - Potsdam',
+    distance: 50
+  },
+  {
+    title : 'Mercedes fuer Retner',
+    description : 'Nur fuer reiche Retner.',
+    price : 32000,
+    place: 'Muenchen',
+    distance: 1000
+  },
+  {
+    title : 'Toyota sushi',
+    description : 'Sushi Master',
+    price : 5000,
+    place: 'Leipzig',
+    distance: 100
+  },
+  {
+    title : 'VW Käfer',
+    description : 'Alteres Fahrzueg',
+    price : 3000,
+    place: 'Berlin - Charlottenburg',
+    distance: 3
+  },
+  {
+    title : 'Rumänische Dacia',
+    description : 'Dacia ist teuerer als Renault.',
+    price : 4500,
+    place: 'Berlin - Tempelhof',
+    distance: 3
+  }
+];
