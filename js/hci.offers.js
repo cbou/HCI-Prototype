2e3a73380e2eaaacbba5e04011e474b8739d7731
Charles <charles@pidoco.com>
2011-06-28 18:59:07 +0200

    add photo to offers

--- js/hci.offers.js
+++ js/hci.offers.js
@@ -4,48 +4,55 @@ hci.offers = [
     description : 'Faehrt gut und schnell',
     price : 16000,
     place: 'Berlin - Wedding',
-    distance: 1
+    distance: 1,
+    photo: 'http://lorempixum.com/80/80/transport/1'
   },
   {
     title : 'Billige Renault',
     description : 'Vier raeder, ein Motor, kein Sitze',
     price : 1500,
     place: 'Erfurt',
-    distance: 500
+    distance: 500,
+    photo: 'http://lorempixum.com/80/80/transport/2'
   },
   {
     title : 'Bequeme Audi',
     description : 'Everyone dreams of an Audi.',
     price : 17000,
     place: 'Berlin - Potsdam',
-    distance: 50
+    distance: 50,
+    photo: 'http://lorempixum.com/80/80/transport/3'
   },
   {
     title : 'Mercedes fuer Retner',
     description : 'Nur fuer reiche Retner.',
     price : 32000,
     place: 'Muenchen',
-    distance: 1000
+    distance: 1000,
+    photo: 'http://lorempixum.com/80/80/transport/4'
   },
   {
     title : 'Toyota sushi',
     description : 'Sushi Master',
     price : 5000,
     place: 'Leipzig',
-    distance: 100
+    distance: 100,
+    photo: 'http://lorempixum.com/80/80/transport/5'
   },
   {
     title : 'VW Käfer',
     description : 'Alteres Fahrzueg',
     price : 3000,
     place: 'Berlin - Charlottenburg',
-    distance: 3
+    distance: 3,
+    photo: 'http://lorempixum.com/80/80/transport/6'
   },
   {
     title : 'Rumänische Dacia',
     description : 'Dacia ist teuerer als Renault.',
     price : 4500,
     place: 'Berlin - Tempelhof',
-    distance: 3
+    distance: 3,
+    photo: 'http://lorempixum.com/80/80/transport/7'
   }
 ];
