07b41d1aea90b517472d58862023a57af79ba8a7
Charles <charles@pidoco.com>
2011-07-12 15:20:47 +0200

    improve namespace

--- js/hci.page.form.js
+++ js/hci.page.form.js
@@ -1,6 +1,6 @@
-hci.formPage = {};
+hci.page.form = {};
 
-hci.formPage.init = function () {
+hci.page.form.init = function () {
   $('form input[type=submit]').click(function(){
    window.location = '/show.html?flashMessage=Ihre Anzeige ist jetzt gespeichert.&flashTitle=Neue Anzeige';
   });
