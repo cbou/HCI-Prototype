649436c369296dec76c90cc332ffbe60f73c882a
Charles <charles@pidoco.com>
2011-07-12 15:29:33 +0200

    make js/css link absolute and add index page javascript

---
+++ js/hci.page.index.js
@@ -0,0 +1,23 @@
+hci.page.index = {};
+
+hci.page.index.init = function () {
+  var galleryWidth = $('.swipegallery').parent().parent().width();
+
+  $('.swipegallery').swipeGallery({
+    height: '80px',
+    width: galleryWidth + 'px',
+    background: '#E2E2E2'
+  });  
+
+  $('.swipegallery-container .swipegallery').css({left: '0px'});
+
+  $('.swipegallery-container').live('swipeleft',function(event, ui){
+    var galleryWidth = $(event.target).parent().parent().width();
+    $('.swipegallery').animate({left: '-='+galleryWidth+'px'}, { duration: 1000 });
+  });
+
+  $('.swipegallery-container').live('swiperight',function(event, ui){
+    var galleryWidth = $(event.target).parent().parent().width();
+    $('.swipegallery').animate({left: '+='+galleryWidth+'px'}, { duration: 1000 });
+  });
+}
