43e8f83306014d7838a9e66487f6b3cd80e03704
Charles <charles@pidoco.com>
2011-07-12 15:57:11 +0200

    set max size to image gallery

--- js/hci.page.form.js
+++ js/hci.page.form.js
@@ -2,6 +2,6 @@ hci.page.form = {};
 
 hci.page.form.init = function () {
   $('form input[type=submit]').click(function(){
-   window.location = '/show.html?flashMessage=Ihre Anzeige ist jetzt gespeichert.&flashTitle=Neue Anzeige';
+    window.location = '/show.html?flashMessage=Ihre Anzeige ist jetzt gespeichert.&flashTitle=Neue Anzeige';
   });
 }
